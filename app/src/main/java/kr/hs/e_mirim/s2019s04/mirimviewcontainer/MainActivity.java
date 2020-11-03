1abbd76c041ab5a76962c25475ce23881b67da66
Kwon Hye Soo <kwon7788966@naver.com>
2020-11-03 19:48:00 +0900

    Initial commit about View Container Test

---
+++ app/src/main/java/kr/hs/e_mirim/s2019s04/mirimviewcontainer/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.e_mirim.s2019s04.mirimviewcontainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
